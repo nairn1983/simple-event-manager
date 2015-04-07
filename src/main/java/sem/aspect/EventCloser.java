ca0c8a17abef755d2404c114c2ee33f4a6529222
Nairn McWilliams <n.mcwilliams@ocado.com>
2015-04-07 14:05:07 +0100

    Change call advice to execution.

--- src/main/java/sem/aspect/EventCloser.java
+++ src/main/java/sem/aspect/EventCloser.java
@@ -9,7 +9,7 @@ import sem.event.EventSubscriber;
 @Aspect
 public class EventCloser {
 
-	@AfterReturning("call(public void *.*(sem.event.Event)) && target(eventSubscriber) && args(event) " +
+	@AfterReturning("execution(public void *.*(sem.event.Event)) && target(eventSubscriber) && args(event) " +
 			"&& @annotation(com.google.common.eventbus.Subscribe)")
 	public void notifyEventCompleted(@SuppressWarnings("unused") final JoinPoint thisJoinPoint, final EventSubscriber eventSubscriber, final Event event) {
 		eventSubscriber.getEventManager().notifyEventCompleted(event);
